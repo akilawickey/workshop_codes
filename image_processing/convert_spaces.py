a9eb6f8b32e3d2a272785b13e305a72ff12b3d31
akila <asak999@gmail.com>
2017-11-12 19:56:26 +0000

    add spaces

--- image_processing/convert_spaces.py
+++ image_processing/convert_spaces.py
@@ -14,10 +14,10 @@ while(1):
     _,frame = cap.read()   
    
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
-    cv2.imshow('frame', grey);
+    cv2.imshow('frame1', grey);
    
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV);
-    cv2.imshow('frame', hsv);
+    cv2.imshow('frame2', hsv);
     # if key pressed is 'Esc' then exit the loop
     if cv2.waitKey(33)== 27:
         break
