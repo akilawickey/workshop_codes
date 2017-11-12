c6b9023cee9a0fb8f373700dfaeb9fc9e99a8c37
Akila Wickey <asak999@gmail.com>
2017-11-13 01:24:44 +0530

    Update convert_spaces.py

--- image_processing/convert_spaces.py
+++ image_processing/convert_spaces.py
@@ -1,1 +1,27 @@
-   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+import cv2
+import numpy as np
+
+# set the web-cam location
+cap = cv2.VideoCapture(0)
+height = 240
+width= 320
+cap.set(3,width)
+cap.set(4,height)
+
+# Loop to continuously get images
+while(1):
+    # Read the frames frome a camera
+    _,frame = cap.read()   
+   
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
+    cv2.imshow('frame', grey);
+   
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV);
+    cv2.imshow('frame', hsv);
+    # if key pressed is 'Esc' then exit the loop
+    if cv2.waitKey(33)== 27:
+        break
+    
+# Clean up and exit the program
+cv2.destroyAllWindows()
+cap.release()
